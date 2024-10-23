cc5950c6c990db4513d56e52d3a3e9ed749eede5
Shan-JCB <71997263@continental.edu.pe>
2024-10-23 17:26:07 -0500

    Version 1.2 RealParking
    Corregido en Web

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use App\Http\Controllers\TarifaController;
 
 Auth::routes();
 
+Route::get('/', function () {return view('auth.login');});
+
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 //Rutas Admin
